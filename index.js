23438c02f73fe99f05e2e66d5c78f6684485a695
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2024-08-25 12:39:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ function calculateAge(birthDate) {
     return age;
 }
 
-app.listen(3000, () => {
-    console.log('API rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
 });
